8486f38d6c107306a39062360a45730ffe9664c1
даша <dmarueva1234@yandex.ru>
2026-03-25 11:09:39 +0300

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv('data/processed/url_dataset_features.csv')
+
+X = df.drop(columns=['url', 'label'])
+y = df['label']
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X, y)
+
+joblib.dump(model, 'ml/model.pkl')
+
+print("Модель обучена")
+#запуск python train_model.py
