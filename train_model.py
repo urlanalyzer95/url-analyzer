84ae4050ca51c2e7d1a0be7f95da2fb400ff32d0
urlanalyzer95 <urlanalyzer95@gmail.com>
2026-03-25 13:12:31 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -7,7 +7,13 @@ df = pd.read_csv('data/processed/url_dataset_features.csv')
 X = df.drop(columns=['url', 'label'])
 y = df['label']
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(
+    n_estimators=50,      # уменьшили с 100 до 50
+    max_depth=10,         # ограничили глубину
+    min_samples_split=5,  # минимальное количество образцов для разделения
+    min_samples_leaf=2,   # минимальное количество образцов в листе
+    random_state=42
+)
 model.fit(X, y)
 
 joblib.dump(model, 'ml/model.pkl')
