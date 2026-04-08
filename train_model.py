c676ddc717e53aeb6a5497fc51d611953bfe829c
даша <dmarueva1234@yandex.ru>
2026-04-08 18:36:33 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,5 +1,3 @@
-# !!ЗАПУСТИТЕ В ТЕРМИНАЛЕ:   python train_model.py
-
 import pandas as pd
 import joblib
 from sklearn.ensemble import RandomForestClassifier
@@ -17,6 +15,27 @@ y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+# safe_urls
+safe_urls = [
+    'https://google.com',
+    'https://yandex.ru',
+    'https://github.com',
+    'https://stackoverflow.com'
+]
+
+from ml.features import extract_features
+
+safe_features = [extract_features(url) for url in safe_urls]
+
+import pandas as pd
+
+safe_df = pd.DataFrame(safe_features, columns=df.drop(columns=['url', 'label']).columns)
+safe_df['label'] = 0
+safe_df['url'] = safe_urls
+
+# Добавляем к датасету
+df = pd.concat([df, safe_df], ignore_index=True)
 
 # 4. Обучаем модель
 model = RandomForestClassifier(
@@ -38,3 +57,5 @@ print(f"Accuracy: {accuracy:.4f}")
 joblib.dump(model, 'ml/model.pkl')
 
 print("Модель сохранена в ml/model.pkl")
+
+
